798217ed45794151274b0c198ce6e8b43525c839
MarcelaM18 <marcelamorales1808@gmail.com>
2023-09-16 10:49:22 -0500

    commit rutas products

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const roleRoutes = require('./routes/roleRoutes');
 const bookRoutes = require('./routes/bookRoutes');
+const productRoutes = require('./routes/productRoutes');
 
 app.use(bodyParser.json());
 
@@ -11,5 +12,6 @@ app.use(bodyParser.json());
 app.use('/api', userRoutes);
 app.use('/api', roleRoutes);
 app.use('/api', bookRoutes);
+app.use('/api', productRoutes);
 
 module.exports = app;
